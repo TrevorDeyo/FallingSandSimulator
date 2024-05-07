4d4cfde5e711e9017254cd25e6ba06ddd088ae75
Deyo <36089323+TrevorDeyo@users.noreply.github.com>
2024-05-07 12:23:46 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,9 +7,11 @@
 int main(int argc, char* argv[]) {
 	SDL_Window* window = nullptr;			// Declare a pointer
 
-	if (SDL_Init(SDL_INIT_VIDEO) < 0) {		// Initialize SDL2 & Check if succesful
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)		// Initialize SDL2 & Check if succesful
+	{
 		std::cout << "SDL could not be initialized: " << SDL_GetError();
-	} else {
+	} 
+	else {
 		std::cout << "SDL video system is ready to go\n";
 	}
 
@@ -24,27 +26,36 @@ int main(int argc, char* argv[]) {
 		"Falling Sand Simulator",   // window title
 		SDL_WINDOWPOS_CENTERED,		// the x position of the window 
 		SDL_WINDOWPOS_CENTERED,		// the y position of the window
-		Width - 100,				// width, in pixels
-		Height - 100,				// height, in pixels
-		SDL_WINDOW_SHOWN);			// flag
+		Width,						// width, in pixels
+		Height,						// height, in pixels
+		SDL_WINDOW_FULLSCREEN_DESKTOP);		// flag
 
 	// Check that the window was successfully created
-	if (window == NULL) {
+	if (window == NULL)
+	{
 		// In the case that the window could not be made...
 		std::cout << "Could not create window: " << SDL_GetError;
 		return 1;
 	}
 
-	bool gameIsRunning = true;
-	while (gameIsRunning) {
+	bool running = true;
+	while (running)
+	{
 		SDL_Event event;
 		// Start the event loop
-		while (SDL_PollEvent(&event)) {
-			if (event.type == SDL_QUIT) {
-				gameIsRunning = false;
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_QUIT)
+			{
+				running = false;
 			}
-			if (event.type == SDL_MOUSEBUTTONDOWN) {
-				std::cout << "mouse has been clicked test" << '\n';
+			else if (event.type == SDL_MOUSEBUTTONDOWN)
+			{
+				std::cout << "mouse button down\n";
+			}
+			else if (event.type == SDL_MOUSEBUTTONUP)
+			{
+				std::cout << "mouse button up\n";
 			}
 		}
 	}
